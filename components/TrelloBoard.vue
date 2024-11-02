47f69e932257227e2ef47859157956b082ec7b65
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2024-11-02 11:25:54 -0500

    3. Mejora en Columnas y LocalStorage

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -2,7 +2,7 @@
 import { nanoid } from 'nanoid';
 import type { Column, Task } from '~/types';
 import draggable from "vuedraggable";
-const columns = ref<Column[]>([
+const columns = useLocalStorage<Column[]>("trelloBoard",[
     {
         id: nanoid(),
         title: "Backlog",
@@ -29,18 +29,30 @@ const columns = ref<Column[]>([
     {id:nanoid(), title: "Complete", tasks:[]},
 ]);
 
+function createColumn(){
+    const column: Column = {
+        id: nanoid(),
+        title: "",
+        tasks: [],
+    }
+    columns.value.push(column);
+    nextTick(() => {
+        (document.querySelector(".column:last-of-type .title-input") as HTMLInputElement).focus();
+    })  
+}
+
 const alt = useKeyModifier("Alt")
 </script>
 
 <template>
-    <div >
+    <div class="flex items-start overflow-x-auto gap-4">
         <draggable
             v-model="columns"
             group="columns"
             :animation="200"
             handle=".drag-handle"
             item-key="id"
-            class="flex gap-4 overflow-x-auto items-start"
+            class="flex gap-4 items-start"
         >
         <template #item="{element: column}: {element: Column}">
             <div
@@ -48,7 +60,16 @@ const alt = useKeyModifier("Alt")
         >
             <header class="font-bold mb-4">
                 <DragHandle/>
-                {{ column.title }}
+                <input 
+                 class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+                 @keyup.enter="($event.target as HTMLInputElement).blur()"
+                 @keydown.backspace="column.title === '' 
+                    ? (columns = columns.filter((c) => c.id !== column.id))
+                    : null
+                 "
+                 type="text"
+                 v-model="column.title"
+                />
             </header>
             <draggable
                 v-model="column.tasks"
@@ -72,6 +93,12 @@ const alt = useKeyModifier("Alt")
         </div>
         </template>
     </draggable>
+    <button
+        @click="createColumn"
+        class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-20"
+    >
+        +Add Column
+    </button>
         
     </div>
 </template>
--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -17,7 +17,7 @@ onKeyStroke('Backspace', (e)=>{
 
 <template>
     <div
-        :title="task.createdAt.toLocaleDateString()"
+        :title="new Date(task.createdAt).toLocaleDateString()"
         class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]"
         @focus="focused = true"
         @blur="focused = false"
